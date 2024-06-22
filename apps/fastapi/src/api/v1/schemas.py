456947da2d115e9c5ee4b2ef8c7eae013c538710
MikhailKop <Mihailkopylov16258@gmail.com>
2024-06-22 22:41:40 +0300

    Fix schemas

--- apps/fastapi/src/api/v1/schemas.py
+++ apps/fastapi/src/api/v1/schemas.py
@@ -1,25 +1,33 @@
 from pydantic import BaseModel
-
 from uuid import UUID
 
 
+class BasePerson(BaseModel):
+    id: UUID
+    full_name: str
+
+class Genre(BaseModel):
+    name: str
+    id: UUID
+    
 class Film(BaseModel):
     id: UUID
     title: str
-    imdb_rating: float
-
-
-class Genre(BaseModel):
-    id: UUID
-    name: str
-
-
-class Person(BaseModel):
-    id: UUID
-    full_name: str
-    films: list['PersonFilmRoles']
+    imdb_rating: float | None
+    description: str | None
+    genres: list[Genre] | None
+    description: str | None
+    directors_names: str | None
+    actors_names: str | None
+    writers_names: str | None
+    directors: list[BasePerson] | None
+    actors: list[BasePerson] | None
+    writers: list[BasePerson] | None
 
 
 class PersonFilmRoles(BaseModel):
     id: UUID
-    roles: list[str]
+    roles: list[str] | None
+    
+class Person(BasePerson):
+    films: list[PersonFilmRoles] | None
