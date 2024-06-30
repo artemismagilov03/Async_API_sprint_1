e5e3a0277cbbfc791bccac47a4d27b2e4064281e
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-06-30 21:06:04 +0400

    drop event, use new method lifespan

--- movies_api/movies_api/main.py
+++ movies_api/movies_api/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
@@ -7,22 +9,21 @@ from movies_api.api.v1 import films, genres, persons
 from movies_api.core.config import settings
 from movies_api.db import elastic, redis
 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis.rd = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
+    elastic.es = AsyncElasticsearch(f'http://{settings.ELASTIC_HOST}:{settings.ELASTIC_PORT}')
+    yield
+    await redis.rd.close()
+    await elastic.es.close()
+
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     default_response_class=ORJSONResponse,
+    lifespan=lifespan,
 )
-
-
-@app.on_event('startup')
-async def startup():
-    redis.rd = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
-    elastic.es = AsyncElasticsearch(f'http://{settings.ELASTIC_HOST}:{settings.ELASTIC_PORT}')
-
-
-@app.on_event('shutdown')
-async def shutdown():
-    await redis.rd.close()
-    await elastic.es.close()
 
 
 app.include_router(films.router)
