54b6fe2a2577a58fe3ac34b7007acd99a4c7553c
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-05 04:09:03 +0400

    async close will be better

--- movies_api/movies_api/main.py
+++ movies_api/movies_api/main.py
@@ -15,6 +15,7 @@ async def lifespan(app: FastAPI):
     redis.rd = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, decode_responses=True)
     elastic.es = AsyncElasticsearch(f'http://{settings.ELASTIC_HOST}:{settings.ELASTIC_PORT}')
     yield
+    await redis.rd.aclose()
     await elastic.es.close()
 
 
